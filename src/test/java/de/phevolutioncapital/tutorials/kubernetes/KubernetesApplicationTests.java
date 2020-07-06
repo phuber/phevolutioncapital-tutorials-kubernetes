e5b02da48287a5ac189ee7c7ca766fe84aa1d9b8
Patrick Huber <patrickhuber@Patricks-MBP.fritz.box>
2020-07-07 01:28:20 +0200

    Initial commit

---
+++ src/test/java/de/phevolutioncapital/tutorials/kubernetes/KubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.phevolutioncapital.tutorials.kubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
